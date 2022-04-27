9bbab58c49272562ff02161fb6e55c84c4408fbc
tatuNo <Tatu.Nordstrom@metropolia.fi>
2022-04-27 14:45:24 +0300

    delete unused route

--- controllers/logout.js
+++ controllers/logout.js
@@ -2,25 +2,13 @@ const router = require('express').Router()
 
 const { tokenExtractor, sessionFinder, userFinder } = require('../util/middleware')
 
-const { Session, User } = require('../models')
-
 router.delete('/', tokenExtractor, userFinder, sessionFinder, async (req, res) => {
 
   req.sessions.forEach(async session => {
     await session.destroy()
   })
+  
   res.status(204).end()
-})
-
-router.get('/', async (req,res) => {
-  const sessions = await Session.findAll({
-    include: {
-      model: User,
-      attributes: ['name', 'username', 'disabled']
-    }
-  })
-
-  res.json(sessions)
 })
 
 module.exports = router
