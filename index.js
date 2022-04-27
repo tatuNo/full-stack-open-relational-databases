5b8ac32673540498e9e855712a6c6bf130456e44
tatuNo <Tatu.Nordstrom@metropolia.fi>
2022-04-27 14:28:15 +0300

    Exercises 17 - 24

--- index.js
+++ index.js
@@ -9,6 +9,8 @@ const blogsRouter = require('./controllers/blogs')
 const usersRouter = require('./controllers/users')
 const loginRouter = require('./controllers/login')
 const authorsRouter = require('./controllers/authors')
+const readingListsRouter = require ('./controllers/readingLists')
+const logoutRouter = require('./controllers/logout')
 
 const { unknownEndpoint, errorHandler } = require('./util/middleware')
 
@@ -18,6 +20,8 @@ app.use('/api/blogs', blogsRouter)
 app.use('/api/users', usersRouter)
 app.use('/api/login', loginRouter)
 app.use('/api/authors', authorsRouter)
+app.use('/api/readinglists', readingListsRouter)
+app.use('/api/logout', logoutRouter)
 
 app.use(unknownEndpoint)
 app.use(errorHandler)
